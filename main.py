d246b1c2fb4c7b434a3552487f659f9be721aca5
hvlgo <qg3035276228@126.com>
2022-12-28 00:24:13 +0800

    modify main.py

--- main.py
+++ main.py
@@ -1,70 +1,12 @@
 import sys
-import re
-import json
 from antlr4 import *
 from src.cpp2llvmLexer import cpp2llvmLexer
 from src.cpp2llvmParser import cpp2llvmParser
+from src.cpp2llvmParserVisitor import cpp2llvmParserVisitor
 
-def toJsonNode(input):
-    dict = {
-        'name': '',
-        'children': []    
-    }
-    # print(input)
-    try:
-        # 获取name
-        # 内部节点
-        if input[0] == '(' and len(input) > 1:
-            dict['name'] = str(re.search(r'\(\S+', input).group(0))[1:]
-        # 叶子节点
-        else:
-            # dict['name'] = str(re.search(r'\S+', input).group(0))
-            dict['name'] = input
-            return dict
-    except Exception as e:
-        print(e)
-        print(input)
-    # print(dict)
-
-    # 解析子节点, 后括号去掉
-    new_str = input[len(dict['name'])+2:-1]
-    # print(new_str)
-
-    stack_total = 0
-    flag = False        # 进入括号的标志
-    left = right = 0
-    for char in new_str:
-        if not flag:
-            if char != ' ':
-                right += 1
-                # 到最后一个
-                if right >= len(new_str):
-                    dict['children'].append(toJsonNode(new_str[left:right]))
-                    return dict
- 
-                if char == '(' :
-                    # 需要解析的
-                    if new_str[right] == ')' or new_str[right] == ' ':
-                        continue
-                    flag = True
-                    stack_total += 1
-                    left = right - 1
-            else:
-                dict['children'].append(toJsonNode(new_str[left:right]))
-                left = right = right + 1
-        else:
-            right += 1
-            if right >= len(new_str):
-                dict['children'].append(toJsonNode(new_str[left:right]))
-                return dict
-            if char == '(' and new_str[right] != ')' and new_str[right] != ' ':
-                stack_total += 1
-            elif char == ')' and new_str[right-2] != ' ':
-                stack_total -= 1
-            
-            if stack_total == 0:
-                flag = False
-    return dict
+class myVisitor(cpp2llvmParserVisitor):
+    def __init__(self):
+        pass
 
 def main(argv):
     input_stream = FileStream(argv[1])
@@ -74,12 +16,15 @@ def main(argv):
     tree = parser.translationUnit()
     ast = tree.toStringTree(recog=parser)
     print(ast)
-    root = toJsonNode(ast)
-    outputfile = "record.json"
+    visitor = myVisitor()
+    visitor.visit(tree)
+    
+    outputfile = "output.ll"
     if len(argv) >= 3:
         outputfile = argv[2]
     with open(outputfile, "w") as f:
-        json.dump(root, f, indent=2, ensure_ascii=False)
+        f.write(str(visitor.Module))
+    
 
 if __name__ == '__main__':
     main(sys.argv)
