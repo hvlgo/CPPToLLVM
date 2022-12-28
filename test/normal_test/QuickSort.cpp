f91e5ce98761c8d789beed1a390c225556aa8161
rjw20020718 <rjw20020718@163.com>
2022-12-28 19:44:51 +0800

    add QuickSort

---
+++ test/normal_test/QuickSort.cpp
@@ -0,0 +1,39 @@
+int printf(char* a, ...);
+int scanf(char* a, ...);
+
+int N;
+
+int q[100];
+
+void quick_sort(int l,int r)
+{
+    int i = l - 1, j = r + 1;
+    int x = q[(l+r)/2];
+    if (l >= r) 
+        return;
+    while (i < j) {
+        do i++; while(q[i] < x);
+        do j--; while(q[j] > x);
+        if(i < j) {
+            int tmp = q[i];
+            q[i] = q[j];
+            q[j] = tmp;
+        }
+            
+    }
+    quick_sort(l, j);
+    quick_sort(j + 1, r);
+}
+    
+int main()
+{   
+    printf("enter N(<100):");
+    scanf("%d", &N);
+    int i;
+    for (i = 0; i < N; i++)
+        scanf("%d", &q[i]);
+    quick_sort(0, N - 1);
+    for (i = 0; i < N; i++)
+        printf("%d ", q[i]);
+    return 0;
+}
